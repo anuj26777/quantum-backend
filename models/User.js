8065fbef0419a7245e6fa2d25ec2aeae9a01f660
Aarti Kumari <kaarti.kc@gmail.com>
2024-12-02 23:19:12 +0530

    m2

--- models/User.js
+++ models/User.js
@@ -1,11 +1,97 @@
-const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  dob: { type: String, required: true },
-  email: { type: String, required: true, unique: true }, 
-  password: { type: String, required: true },
-});
+const { model , Schema} = require('mongoose');
+const mongoose  = require('mongoose');
 
-module.exports = mongoose.model("User", UserSchema);
+const addressSchema = new Schema ( {
+  street:{
+    type:String,
+    required:true
+  },
+  city:{
+    type:String,
+    required:true
+  },
+  state:{
+    type:String,
+    required:true
+  },
+  zipCode:{
+    type:String,
+    required:true
+  },
+  country:{
+    type:String,
+    required:true
+  }
+})
 
+const userSchema  = new Schema ( {
+
+  name : {
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true
+  },
+  phone:{
+    type:String,
+    required:true
+  },
+
+  role: {
+    type: String,
+    enum: ["admin", "customer"],
+    default: "customer",
+  },
+
+  addresses: [addressSchema],
+
+  wishlist: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+    },
+  ],
+
+  orderHistory: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+  ],
+
+  isVerified:{
+    type: Boolean,
+   default: false
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+
+  updatedAt: { 
+    type: Date, 
+    default: Date.now 
+  },
+
+},
+{
+  timestamps: true
+}
+
+);
+
+
+const userModel = model("User" , userSchema);
+
+
+
+module.exports = {
+  userModel
+}
